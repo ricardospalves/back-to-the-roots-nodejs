6ad88b030ec0fb508bc266789ceeba06195874f1
Ricardo Alves <ricardospalves@gmail.com>
2024-06-07 01:04:16 -0300

    feat(api-restfull): add GetBooksController

---
+++ api-resfull/controller/books/getBooksController.js
@@ -0,0 +1,20 @@
+import { DEFAULT_HEADER } from "../../constants/defaultHeader.js";
+
+export class GetBooksController {
+  constructor(getBooksUseCase) {
+    this.getBooksUseCase = getBooksUseCase;
+  }
+
+  handle(request, response) {
+    const books = this.getBooksUseCase.execute();
+
+    response.writeHead(201, DEFAULT_HEADER);
+    response.write(
+      JSON.stringify({
+        results: books,
+      })
+    );
+
+    return response.end();
+  }
+}
