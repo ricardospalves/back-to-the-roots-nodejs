05c2bd381b5e36c8b2ffdf7e07819996925bea7b
Ricardo Alves <ricardospalves@gmail.com>
2024-06-08 03:30:40 -0300

    fix(api-restfull): rename parameters of booksGet

--- api-resfull/routes/books/booksGet.js
+++ api-resfull/routes/books/booksGet.js
@@ -3,11 +3,11 @@ import { BooksRepository } from "../../repositories/bookRepository.js";
 import { BooksService } from "../../services/booksService.js";
 import { GetBooksUseCase } from "../../useCases/books/getBooksUseCase.js";
 
-export const booksGet = (response, request) => {
+export const booksGet = (request, response) => {
   const repository = new BooksRepository();
   const service = new BooksService({ booksRepository: repository });
   const useCase = new GetBooksUseCase(service);
   const controller = new GetBooksController(useCase);
 
-  return controller.handle(response, request);
+  return controller.handle(request, response);
 };
