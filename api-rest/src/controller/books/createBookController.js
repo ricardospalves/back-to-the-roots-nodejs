f4d38e576fe53e6954fb8925586748555047414b
Ricardo Alves <ricardospalves@gmail.com>
2024-07-31 00:17:47 -0300

    feat(api-rest): add date type validation to CreateBookController

--- api-rest/src/controller/books/createBookController.js
+++ api-rest/src/controller/books/createBookController.js
@@ -1,6 +1,8 @@
 import { once } from "node:events";
 import { DEFAULT_HEADER } from "../../constants/defaultHeader.js";
 import { BookEntity } from "../../entities/bookEntity.js";
+import { realTypeof } from "../../utils/realTypeof.js";
+import { RESPONSE_ERRORS } from "../../constants/responseErrors.js";
 
 export class CreateBookController {
   constructor(createBookUseCase) {
@@ -10,6 +12,11 @@ export class CreateBookController {
   async handle(request, response) {
     const data = await once(request, "data");
     const body = JSON.parse(data);
+
+    if (realTypeof(body) !== "object") {
+      throw new Error(RESPONSE_ERRORS.invalidDataType.id);
+    }
+
     const book = new BookEntity(body);
     const bookID = await this.createBookUseCase.execute(book);
 
