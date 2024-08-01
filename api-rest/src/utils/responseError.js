fae85aa563a402224fef35644d6eedd8fe00c5ba
Ricardo Alves <ricardospalves@gmail.com>
2024-08-01 00:06:34 -0300

    fix(rest-api): remove status and message from responseError

--- api-rest/src/utils/responseError.js
+++ api-rest/src/utils/responseError.js
@@ -1,11 +1,11 @@
 import { DEFAULT_HEADER } from "../constants/defaultHeader.js";
 
 export const responseError = (response, { status, message }) => {
-  response.writeHead(500, DEFAULT_HEADER);
+  response.writeHead(status, DEFAULT_HEADER);
   response.write(
     JSON.stringify({
       error: true,
-      message: "Internal server error.",
+      message,
     })
   );
 
