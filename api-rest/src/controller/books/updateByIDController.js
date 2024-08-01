d6e122f0c500b39c227da2fa72b17811f7292a45
Ricardo Alves <ricardospalves@gmail.com>
2024-08-01 00:09:03 -0300

    feat(rest-api): add validations to UpdateBookByIDController

--- api-rest/src/controller/books/updateByIDController.js
+++ api-rest/src/controller/books/updateByIDController.js
@@ -1,5 +1,7 @@
 import { once } from "node:events";
 import { DEFAULT_HEADER } from "../../constants/defaultHeader.js";
+import { RESPONSE_ERRORS } from "../../constants/responseErrors.js";
+import { realTypeof } from "../../utils/realTypeof.js";
 
 export class UpdateBookByIDController {
   constructor(updateByIDUseCase) {
@@ -8,10 +10,18 @@ export class UpdateBookByIDController {
 
   async handle(request, response) {
     const { id: bookID } = request.params;
-    const data = await once(request, "data");
-    const book = JSON.parse(data);
+    const body = await once(request, "data");
+    const book = JSON.parse(body);
 
-    await this.updateByIDUseCase.execute(bookID, book);
+    if (realTypeof(book) !== "object") {
+      throw new Error(RESPONSE_ERRORS.invalidDataType.id);
+    }
+
+    const updatedBook = await this.updateByIDUseCase.execute(bookID, book);
+
+    if (!updatedBook) {
+      throw new Error(RESPONSE_ERRORS.bookNotFound.id);
+    }
 
     response.writeHead(200, DEFAULT_HEADER);
     response.write(
